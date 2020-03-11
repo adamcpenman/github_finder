5854e01fa9496a0e625cadd84f64161c92849166
Adam <adamcpenman@gmail.com>
2020-03-11 17:31:30 -0500

    search, going over class and functional components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,71 @@
 import React, { Component } from 'react';
+import NavBar from './Components/Layout/NavBar';
+import Users from './Components/Users/Users';
+import Search from './Components/Users/Search';
+import Alert from './Components/Layout/Alert';
+import axios from 'axios';
 import './App.css';
 
-class App extends Component{
+class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+    alert: null
+  };
+
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=
+  //     ${process.env.REACT_APP_GITHUB_CLIENT_ID}
+  //     &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
+
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=
+      ${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 5000);
+  };
+
   render() {
-     return (
-    <div className="App">
-     <h1>Hello from React</h1>
-    </div>
-  );
+    const { users, loading } = this.state;
+    return (
+      <div>
+        <nav className='navbar bg-primary'>
+          <NavBar title='Github Finder' icon='fab fa-github' />
+        </nav>
+        <div className='container'>
+          <Alert alert={this.state.alert} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
+          <Users loading={loading} users={users} />
+        </div>
+      </div>
+    );
   }
- }
+}
 
 export default App;
---
+++ src/Components/Layout/NavBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const NavBar = ({ icon, title }) => {
+  return (
+    <div>
+      <h1>
+        <i className={icon} />
+        {title}
+      </h1>
+    </div>
+  );
+};
+
+NavBar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fab fa-github'
+};
+
+NavBar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
+
+export default NavBar;
